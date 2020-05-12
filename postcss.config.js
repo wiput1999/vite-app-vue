947812f42fffc043116879c49fa9bab1d86c3529
Wiput Pootong <wiput.pootong@gmail.com>
2020-05-13 00:42:27 +0700

    :tada: initial commit

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+const purgeCss =
+  process.env.NODE_ENV === 'production'
+    ? [
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}',
+          ],
+          defaultExtractor: (content) =>
+            content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        },
+      ]
+    : undefined
+
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'postcss-preset-env',
+    purgeCss,
+    ['autoprefixer', { grid: true }],
+    'cssnano',
+  ],
+}
